db679123370a34e3735d0a168a81d5481d0d3513
tsubasa <your_email@examples.com>
2024-03-08 22:49:32 +0900

    Update: typing-game.js

--- Trial_production/typing_game/typing-game.js
+++ Trial_production/typing_game/typing-game.js
@@ -14,11 +14,22 @@ const remained = document.getElementById('remained');
 const inputText = document.getElementById('inputText');
 
 let remainedTextWords = remained.textContent.split('');
-console.log(remainedTextWords);
+let enteredTextWords = [];
 
 document.addEventListener('input',(e) => {
   if(remainedTextWords[0] === e.data){
-    console.log('正解');
+
+    // 入力済み文字の配列の最後に１文字追加
+    enteredTextWords.push ( remainedTextWords[0] );
+    // 未入力文字の履いてるの先頭から1文字削除
+    remainedTextWords.shift();
+
+    console.log('入力済み' + enteredTextWords);
+    console.log('未入力' + remainedTextWords);
+
+    // 入力済みテキスト&未入力テキストを連結して画面表示
+    entered.textContent = enteredTextWords.join('');
+    remained.textContent = remainedTextWords.join('');
   } else {
     console.log('不正解');
   }

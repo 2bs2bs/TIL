c918a4d01be507d12866b5b2c8e05d6e10513dd4
tsubasa <tsubasa19957629@au.com>
2024-05-12 23:50:00 +0900

    Add: gsub

--- Ruby/Algorithms_for_ruby/next_string.rb
+++ Ruby/Algorithms_for_ruby/next_string.rb
@@ -1,17 +1,39 @@
-str = gets.chomp
 def decode_message(message)
-    decode_message = ""
-    prev_char = nil
-    
-    message.each_char do |char|
-        if char.match?(/\d/)
-            decode_message += (char.to_i + 1).to_s
-        elsif char.match?(/[A-Z]/)
-            decode_message += (char.ord - 64 + 1).chr
-        end
+  decoded_message = ""
+  current_number = ""
+
+  message.each_char do |char|
+    if char.match?(/\d/)
+      current_number += char
+    elsif char.match?(/[A-Z]/)
+      # 数字が2つ以上連続している場合、1つの数字として処理
+      decoded_message += (current_number.to_i + 1).to_s unless current_number.empty?
+      current_number = ""  # 数字をリセット
+
+      decoded_char = char == 'Z' ? 'A' : (char.ord + 1).chr
+      decoded_message += decoded_char
     end
-    
-    return decode_message
+  end
+
+  # メッセージの末尾に数字が残っている場合の処理
+  decoded_message += (current_number.to_i + 1).to_s unless current_number.empty?
+
+  decoded_message
 end
 
-puts decode_message(str)
+# メッセージを入力
+message = gets.chomp
+
+# 解読したメッセージを出力
+puts decode_message(message)
+
+= begin
+1st "","P"
+2nd "","PA"
+3rd "","PAI"
+4th "","PAIZ"
+5th "","PAIZA"
+6th "3","PAIZA"
+7th "38","PAIZA"
+PAIZA39
+= end

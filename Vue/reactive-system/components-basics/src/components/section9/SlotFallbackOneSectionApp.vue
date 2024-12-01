1c5c3b25258d7b227c60fe0be89bdbfbf9c9fbe5
tsubasa <tsubasa19957629@au.com>
2024-12-01 10:35:38 +0900

    Add: 遊び

---
+++ Vue/reactive-system/components-basics/src/components/section9/SlotFallbackOneSectionApp.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h2>Slotの利用</h2>
+    <SlotFallbackOneSection v-bind:name="taro">
+      <p>連絡がつきません</p>
+    </SlotFallbackOneSection>
+    <SlotFallbackOneSection v-bind:name="jiro"></SlotFallbackOneSection>
+  </section>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+import SlotFallbackOneSection from "./SlotFallbackOneSection.vue";
+
+const taro = ref("田中太郎");
+const jiro = ref("鈴木二郎");
+</script>
